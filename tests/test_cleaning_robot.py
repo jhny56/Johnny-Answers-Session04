9d7367ca3e024aa654e22bde11e69e0495d10a13
johnny <johnny-hanna@outlook.com>
2024-07-12 18:23:46 +0300

    Keep only gpt tests

--- tests/test_cleaning_robot.py
+++ tests/test_cleaning_robot.py
@@ -1,18 +1,3 @@
-from src.robots.cleaning_robot import CleaningRobot
-
-def testCleaningRobot():
-
-    testrobot = CleaningRobot("testname",1000,"idle", "Choffata")
-
-    print(testrobot.get_cleaning_tool())
-
-    testrobot.work()
-    testrobot.report_status()
-    testrobot.set_cleaning_tool("messe7a")
-    testrobot.report_status()
-    
-    testrobot.self_diagnose()
-
 import unittest
 from src.robots.cleaning_robot import CleaningRobot
 from test_base_robot import TestBaseRobot
